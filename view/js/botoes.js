6a8a69c12c2c245017fcfc2e7f29230996b281ee
Elvis Douglas Janegiz <edjelvis@gmail.com>
2018-09-10 20:27:00 -0300

    Adicionando validação para botões das telas

--- view/js/botoes.js
+++ view/js/botoes.js
@@ -20,48 +20,148 @@ const limpaCampos = () => {
 }
 
 const pressionaBotao = (id) => {
-  if (listaNumeros.length < 5) {
+  if (candidato === 'Deputado Estadual') {
+    if (listaNumeros.length < 5) {
 
-    botao = document.getElementById(id).innerText
-    listaNumeros.push(botao)
+      botao = document.getElementById(id).innerText
+      listaNumeros.push(botao)
 
-    console.log(listaNumeros)
+      console.log(listaNumeros)
 
-    for (let i in listaNumeros) {
-      if (listaNumeros.length === 1) {
-        document.getElementById('dig1').innerHTML = listaNumeros[0]
-        document.getElementById('dig2').innerHTML = ''
-        document.getElementById('dig3').innerHTML = ''
-        document.getElementById('dig4').innerHTML = ''
-        document.getElementById('dig5').innerHTML = ''
+      for (let i in listaNumeros) {
+        if (listaNumeros.length === 1) {
+          document.getElementById('dig1').innerHTML = listaNumeros[0]
+          document.getElementById('dig2').innerHTML = ''
+          document.getElementById('dig3').innerHTML = ''
+          document.getElementById('dig4').innerHTML = ''
+          document.getElementById('dig5').innerHTML = ''
+        }
+        else if (listaNumeros.length === 2) {
+          document.getElementById('dig1').innerHTML = listaNumeros[0]
+          document.getElementById('dig2').innerHTML = listaNumeros[1]
+          document.getElementById('dig3').innerHTML = ''
+          document.getElementById('dig4').innerHTML = ''
+          document.getElementById('dig5').innerHTML = ''
+        }
+        else if (listaNumeros.length === 3) {
+          document.getElementById('dig1').innerHTML = listaNumeros[0]
+          document.getElementById('dig2').innerHTML = listaNumeros[1]
+          document.getElementById('dig3').innerHTML = listaNumeros[2]
+          document.getElementById('dig4').innerHTML = ''
+          document.getElementById('dig5').innerHTML = ''
+        }
+        else if (listaNumeros.length === 4) {
+          document.getElementById('dig1').innerHTML = listaNumeros[0]
+          document.getElementById('dig2').innerHTML = listaNumeros[1]
+          document.getElementById('dig3').innerHTML = listaNumeros[2]
+          document.getElementById('dig4').innerHTML = listaNumeros[3]
+          document.getElementById('dig5').innerHTML = ''
+        }
+        else if (listaNumeros.length === 5) {
+          document.getElementById('dig1').innerHTML = listaNumeros[0]
+          document.getElementById('dig2').innerHTML = listaNumeros[1]
+          document.getElementById('dig3').innerHTML = listaNumeros[2]
+          document.getElementById('dig4').innerHTML = listaNumeros[3]
+          document.getElementById('dig5').innerHTML = listaNumeros[4]
+        }
       }
-      else if (listaNumeros.length === 2) {
-        document.getElementById('dig1').innerHTML = listaNumeros[0]
-        document.getElementById('dig2').innerHTML = listaNumeros[1]
-        document.getElementById('dig3').innerHTML = ''
-        document.getElementById('dig4').innerHTML = ''
-        document.getElementById('dig5').innerHTML = ''
+    }
+  } else if (candidato === 'Deputado Federal') {
+    if (listaNumeros.length < 4) {
+      botao = document.getElementById(id).innerText
+      listaNumeros.push(botao)
+
+      console.log(listaNumeros)
+
+      for (let i in listaNumeros) {
+        if (listaNumeros.length === 1) {
+          document.getElementById('df1').innerHTML = listaNumeros[0]
+          document.getElementById('df2').innerHTML = ''
+          document.getElementById('df3').innerHTML = ''
+          document.getElementById('df4').innerHTML = ''
+        }
+        else if (listaNumeros.length === 2) {
+          document.getElementById('df1').innerHTML = listaNumeros[0]
+          document.getElementById('df2').innerHTML = listaNumeros[1]
+          document.getElementById('df3').innerHTML = ''
+          document.getElementById('df4').innerHTML = ''
+        }
+        else if (listaNumeros.length === 3) {
+          document.getElementById('df1').innerHTML = listaNumeros[0]
+          document.getElementById('df2').innerHTML = listaNumeros[1]
+          document.getElementById('df3').innerHTML = listaNumeros[2]
+          document.getElementById('df4').innerHTML = ''
+        }
+        else if (listaNumeros.length === 4) {
+          document.getElementById('df1').innerHTML = listaNumeros[0]
+          document.getElementById('df2').innerHTML = listaNumeros[1]
+          document.getElementById('df3').innerHTML = listaNumeros[2]
+          document.getElementById('df4').innerHTML = listaNumeros[3]
+        }
       }
-      else if (listaNumeros.length === 3) {
-        document.getElementById('dig1').innerHTML = listaNumeros[0]
-        document.getElementById('dig2').innerHTML = listaNumeros[1]
-        document.getElementById('dig3').innerHTML = listaNumeros[2]
-        document.getElementById('dig4').innerHTML = ''
-        document.getElementById('dig5').innerHTML = ''
+    }
+  } else if (candidato === 'Senador') {
+    if (listaNumeros.length < 3) {
+
+      botao = document.getElementById(id).innerText
+      listaNumeros.push(botao)
+
+      console.log(listaNumeros)
+
+      for (let i in listaNumeros) {
+        if (listaNumeros.length === 1) {
+          document.getElementById('s1').innerHTML = listaNumeros[0]
+          document.getElementById('s2').innerHTML = ''
+          document.getElementById('s3').innerHTML = ''
+        }
+        else if (listaNumeros.length === 2) {
+          document.getElementById('s1').innerHTML = listaNumeros[0]
+          document.getElementById('s2').innerHTML = listaNumeros[1]
+          document.getElementById('s3').innerHTML = ''
+        }
+        else if (listaNumeros.length === 3) {
+          document.getElementById('s1').innerHTML = listaNumeros[0]
+          document.getElementById('s2').innerHTML = listaNumeros[1]
+          document.getElementById('s3').innerHTML = listaNumeros[2]
+        }
       }
-      else if (listaNumeros.length === 4) {
-        document.getElementById('dig1').innerHTML = listaNumeros[0]
-        document.getElementById('dig2').innerHTML = listaNumeros[1]
-        document.getElementById('dig3').innerHTML = listaNumeros[2]
-        document.getElementById('dig4').innerHTML = listaNumeros[3]
-        document.getElementById('dig5').innerHTML = ''
+    }
+  } else if (candidato === 'Governador') {
+    if (listaNumeros.length < 2) {
+
+      botao = document.getElementById(id).innerText
+      listaNumeros.push(botao)
+
+      console.log(listaNumeros)
+
+      for (let i in listaNumeros) {
+        if (listaNumeros.length === 1) {
+          document.getElementById('g1').innerHTML = listaNumeros[0]
+          document.getElementById('g2').innerHTML = ''
+        }
+        else if (listaNumeros.length === 2) {
+          document.getElementById('g1').innerHTML = listaNumeros[0]
+          document.getElementById('g2').innerHTML = listaNumeros[1]
+        }
       }
-      else if (listaNumeros.length === 5) {
-        document.getElementById('dig1').innerHTML = listaNumeros[0]
-        document.getElementById('dig2').innerHTML = listaNumeros[1]
-        document.getElementById('dig3').innerHTML = listaNumeros[2]
-        document.getElementById('dig4').innerHTML = listaNumeros[3]
-        document.getElementById('dig5').innerHTML = listaNumeros[4]
+    }
+  } else if (candidato === 'Presidente') {
+    if (listaNumeros.length < 2) {
+
+      botao = document.getElementById(id).innerText
+      listaNumeros.push(botao)
+
+      console.log(listaNumeros)
+
+      for (let i in listaNumeros) {
+        if (listaNumeros.length === 1) {
+          document.getElementById('p1').innerHTML = listaNumeros[0]
+          document.getElementById('p2').innerHTML = ''
+        }
+        else if (listaNumeros.length === 2) {
+          document.getElementById('p1').innerHTML = listaNumeros[0]
+          document.getElementById('p2').innerHTML = listaNumeros[1]
+        }
       }
     }
   }
@@ -87,6 +187,7 @@ const confirma = () => {
       candidato = ''
   }
   manipulaTelas()
+  limpaCampos()
 }
 
 const manipulaTelas = () => {
@@ -114,7 +215,7 @@ const corrige = () => {
 }
 
 const branco = () => {
+  confirma()
   limpaCampos()
   numeroCandidatoPresidente = 'BRANCO'
-  audio.play()
 }
