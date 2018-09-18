23fc57d48e2036271fb912a7bf16fba75d22008d
Felipe Rodrigo <lipedigo@hotmail.com>
2018-09-18 19:22:48 -0300

    Criando tela Titulo

--- view/titulo.php
+++ view/titulo.php
@@ -20,16 +20,19 @@ try {
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" type="text/css" media="screen" href="css/main.css" />
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
- 
+
 </head>
 <body>
     <div class="container">
-        <div class="row">
-            <div class="col-md-3"></div>
-            <div class="col-md-6">
-                <img style="width:100%" src="file/título-eleitor.jpg" alt="">
-            </div>
-        </div>
+        <img id="imgTitulo" src="img/título-eleitor.jpg" alt="">
+        <input id="nomeEleitor" type="text" value="Felipe Rodrigo da Silva">
+        <input id="dataNasc" type="text" value="19/10/1996">
+        <input id="numInscricao" type="text" value="688033403/61">
+        <input id="zona" type="text" value="114">
+        <input id="sessao" type="text" value="0087">
+        <input id="municipio" type="text" value="Sarandi/PR">
+        <input id="dataEmissao" type="text" value="18/05/2014">
+        
     </div>
     
     <script src="js/resultados.js"></script>
