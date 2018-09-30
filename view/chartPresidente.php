e2fefe448424f9cb386e1a3eb71a8798d3f5fea8
Elvis Douglas Janegitz <edjelvis@gmail.com>
2018-09-30 17:42:09 -0300

    Corrigindo titulos das páginas de gráficos de votos

--- view/chartPresidente.php
+++ view/chartPresidente.php
@@ -24,7 +24,7 @@ echo "<script>let listaVotos = " . $returnConvertidoJSON . "
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
+  <title>Votos - Presidentes</title>
   <link rel="stylesheet" type="text/css" media="screen" href="css/main.css" />
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
   
