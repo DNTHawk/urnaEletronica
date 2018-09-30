c813697eee58433dbb4051ee72047c224d980aea
Elvis Douglas Janegitz <edjelvis@gmail.com>
2018-09-30 15:51:14 -0300

    Alterando para voltar para validaCodigo após gerar o código

--- view/chartSenador.php
+++ view/chartSenador.php
@@ -63,7 +63,7 @@ echo "<script>let listaVotos = " . $returnConvertidoJSON . "
               data: {
                 labels: nomeCandidatosSenador,
                 datasets: [{
-                  label: '# of Votes',
+                  label: '# de Votos',
                   data: qtdVotosSenador,
                   backgroundColor: [
                     'rgba(255, 99, 132, 0.2)',
