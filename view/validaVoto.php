01ab66de19745755eea1c88ea24c5ad2326f1e27
Elvis Douglas Janegitz <edjelvis@gmail.com>
2018-09-30 17:54:28 -0300

    Adicionando estilo nos campos do login e removendo titulo.php

--- view/validaVoto.php
+++ view/validaVoto.php
@@ -23,6 +23,16 @@ $codigo = $_SESSION['codigo'];
   <link rel="stylesheet" type="text/css" media="screen" href="css/main.css" />
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
   <script src="js/validaCPF.js"></script>
+
+  <style>
+    .text-label {
+      background-color: #226dbc;
+      color: #FFF;
+      padding: 6px;
+      border-radius: 5px;
+      border: 1px solid #FFF
+    }
+  </style>
 </head>
 <body>
   <div class="bg"></div>
@@ -30,14 +40,14 @@ $codigo = $_SESSION['codigo'];
     <form id="form" action="../system/valida.php" method="POST">
       <div class="row">
         <div class="col-md-6 offset-md-3">
-          <label>CPF:
+          <label class="text-label">CPF:
             <input type="text" name="cpf" class="form-control" onkeydown="javascript: fMasc( this, mCPF );" maxlength="14" placeholder="Digite seu CPF" required>
           </label>
         </div>
       </div>
       <div class="row" style="margin-top:10px">
         <div class="col-md-6 offset-md-3">
-          <label style="color: #FFF">Código:
+          <label class="text-label">Código:
             <input class='form-control' name='codigo' type='text' id='campoCodigo' require value="<?php echo $codigo ?>">
           </label>
         </div>
